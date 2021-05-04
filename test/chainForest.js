75fcfc00825ee76c1997050bbb6c4bc3a1061963
Human Body <72944133+Universe-art@users.noreply.github.com>
2021-05-04 15:47:17 +0200

    Add files via upload

--- test/chainForest.js
+++ test/chainForest.js
@@ -1,12 +1,31 @@
-const BlockchainForestDefi = artifacts.require("BlockchainForestDefi");
+const BlockchainForestDefi1 = artifacts.require("BlockchainForestDefi");
 
+const BlockchainForestDefi2 = artifacts.require("BlockchainForestDefi");
 
 let daiAmount = 1;
 
+
+
+
 contract("BlockchainForestDefi", accounts => {
-  it("should swap Dai to Usdc", () =>
-    BlockchainForestDefi.deployed()
-      .then(instance => instance.method.swapHalfDaiToUsdc(daiAmount).send(accounts[0]))
-      .then(console.log("OK"))
-        );
-      });
+  const sender = accounts[0];
+    it('should swap Dai to Usdc', async function () {
+      BlockchainForestDefi1.deployed()
+        .then(async function(blockchainForestDefiTest) {
+          blockchainForestDefiTest = temp;
+          await blockchainForestDefiTest.swapHalfDaiToUsdc(daiAmount,{ from: sender });
+        })
+        .then(() => done());
+    });
+
+    //  it('should add liquidity To Dai Usdt', async function () {
+    //    BlockchainForestDefi2.deployed()
+    //      .then(async function(blockchainForestDefi) {
+    //        blockchainForestDefi = temp;
+    //        await blockchainForestDefi.addliquidityToDaiUsdt(daiAmount,{ from: sender });
+    //      })
+    //      .then(() => done());
+    //  });
+
+});
+
